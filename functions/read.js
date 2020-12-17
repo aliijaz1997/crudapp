04bddae8e2bc71dacf820f732ea0c9d4d0c1fea0
Hifz ur Rehman Ali <aliijaz2010@yahoo.com>
2020-12-18 03:36:23 +0500

    Update read.js

--- functions/read.js
+++ functions/read.js
@@ -23,7 +23,7 @@ exports.handler = async (event, context) => {
     // ok
     return {
       statusCode: 200,
-      body: JSON.stringify(result.data),
+      body: JSON.stringify(result),
 
     }
   } catch (error) {
